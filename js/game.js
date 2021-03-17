b274720b5a7073c93632bc0e1774a08e406f02dd
diegodalairac <ddalairac@santadertecnologia.com.ar>
2021-03-17 11:49:33 -0300

    configuration to npm, tsc & scss

--- js/game.js
+++ js/game.js
@@ -1,28 +1,5 @@
-var canvas = document.getElementById("stage");
-var ctx = canvas.getContext("2d");
-
-// Full screen
-// ctx.canvas.width = window.innerWidth;
-// ctx.canvas.height = window.innerHeight;
-
-
-// Dibujar elemento
-x = 0
-function draw() {
-    ctx.beginPath();
-    ctx.rect(x, 20, 130, 200);
-    ctx.stroke();
+export class Game {
+    constructor() {
+    }
 }
-
-// Dibujar frame
-function frameLoop() {
-    // borrar canvas
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    // Dibujar canvas
-    draw()
-    if (x > canvas.width) x = -130
-    x++
-}
-
-var interval = setInterval(frameLoop, 10);
-
+//# sourceMappingURL=game.js.map
